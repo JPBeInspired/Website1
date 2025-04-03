65e7f123adab880946928900216b8129849947d9
JPBeInspired <jakep@beinspired.fitness>
2025-04-03 15:55:00 +1100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0f5f0',
+          100: '#d8e6d7',
+          200: '#A8C3A5',  // Our main brand color
+          300: '#8aad86',
+          400: '#6c976a',
+          500: '#557b53',
+          600: '#446343',
+          700: '#324a32',
+          800: '#203220',
+          900: '#0f1910',
+        },
+      },
+    },
   },
   plugins: [],
 };
